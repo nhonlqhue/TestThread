5a05d431a54073e49a177687ca76aa69f2edf93f
nhonlqhue <58057539+nhonlqhue@users.noreply.github.com>
2019-11-22 10:23:52 +0700

    1023

--- Program.cs
+++ Program.cs
@@ -69,7 +69,7 @@ namespace ThreadTracker
                     thread.Thread.Name = i.ToString();
                     thread.Thread.Start();
                 }
-                Task delay = Task.Delay(60000);
+                Task delay = Task.Delay(1*60000);
                 delay.Wait();
             }
         }
